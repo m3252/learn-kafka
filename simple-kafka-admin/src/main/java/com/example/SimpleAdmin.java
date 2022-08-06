0c5bfb4889b77cc9492dd6d51dea427e0222358c
noah <m3252@naver.com>
2022-08-06 19:58:23 +0900

    refactor : 불필요한 코드 정리

--- simple-kafka-admin/src/main/java/com/example/SimpleAdmin.java
+++ simple-kafka-admin/src/main/java/com/example/SimpleAdmin.java
@@ -2,24 +2,17 @@ package com.example;
 
 import org.apache.kafka.clients.admin.*;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
-import org.apache.kafka.clients.consumer.ConsumerRecord;
-import org.apache.kafka.clients.consumer.ConsumerRecords;
-import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.apache.kafka.common.Node;
 import org.apache.kafka.common.config.ConfigResource;
-import org.apache.kafka.common.serialization.StringDeserializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.time.Duration;
 import java.util.*;
 import java.util.concurrent.ExecutionException;
 
 public class SimpleAdmin {
     private final static Logger logger = LoggerFactory.getLogger(SimpleAdmin.class);
-    private final static String TOPIC_NAME = "test";
     private final static String BOOTSTRAP_SERVERS = "my-kafka:9092";
-    private final static String GROUP_ID = "test-group";
 
     public static void main(String[] args) throws ExecutionException, InterruptedException {
         AdminClient admin = AdminClient.create(getProperties());
